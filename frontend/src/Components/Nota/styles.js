87d3638eb77ecb7cbd71c25c3320e14a8eff1276
Gustavo Cabral <studiogustavocabral@gmail.com>
2021-06-22 21:39:34 -0300

    Frontend

---
+++ frontend/src/Components/Nota/styles.js
@@ -0,0 +1,29 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  width: 320px;
+  margin-bottom: 30px;
+  background: #fff;
+  box-shadow: 0 0 14px 0 rgba(0,0,0,0.2);
+  border-radius: 10px;
+  padding: 30px 20px;
+
+  textarea { 
+    width: 100%;
+    padding: .5rem 0;
+    font-size: 14px;
+    border: 0;
+    margin: 1rem 0rem 1rem;
+    resize: none;
+ 
+  }
+.li-nota { 
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  
+ 
+}
+
+
+` 
